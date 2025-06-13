5cb31bcfd8a17047e996b734a1f63bb2c2986b31
Tema <pshenichnykh.art@yandex.com>
2025-06-13 10:58:16 +0800

    Create Auth.cs

---
+++ FinamAPI/Models/Auth.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FinamAPI.Models
+{
+    public class AuthRequest
+    {
+        [JsonPropertyName("grant_type")]
+        public string GrantType { get; set; } = "client_credentials";
+
+        [JsonPropertyName("client_id")]
+        public string ClientId { get; set; }
+
+        [JsonPropertyName("client_secret")]
+        public string ClientSecret { get; set; }
+    }
+    public class AuthResponse
+    {
+        [JsonPropertyName("access_token")]
+        public string AccessToken { get; set; }
+
+        [JsonPropertyName("token_type")]
+        public string TokenType { get; set; } = "Bearer";
+
+        [JsonPropertyName("expires_in")]
+        public int ExpiresIn { get; set; }
+
+        [JsonPropertyName("scope")]
+        public string Scope { get; set; }
+
+        [JsonIgnore]
+        public DateTime TokenExpiration { get; set; }
+
+        [JsonIgnore]
+        public bool IsSuccess => !string.IsNullOrEmpty(AccessToken);
+    }
+
+    public class AuthErrorResponse
+    {
+        [JsonPropertyName("error")]
+        public string Error { get; set; }
+
+        [JsonPropertyName("error_description")]
+        public string ErrorDescription { get; set; }
+    }
+}
