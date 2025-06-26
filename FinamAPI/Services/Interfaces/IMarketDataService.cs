70c1ea639fe615acb8b911569239286c68b6f3f5
Tema <pshenichnykh.art@yandex.com>
2025-06-26 21:22:54 +0800

    Create IMarketDataService.cs

---
+++ FinamAPI/Services/Interfaces/IMarketDataService.cs
@@ -0,0 +1,9 @@
+﻿using FinamAPI.Models;
+
+namespace FinamAPI.Services.Interfaces
+{
+    public interface IMarketDataService
+    {
+        Task<ApiResponse<List<Security>>> GetSecuritiesAsync();
+        Task<ApiResponse<List<Candle>>> GetCandlesAsync(string securityCode, DateTime from, DateTime to, string timeframe);
+    }
