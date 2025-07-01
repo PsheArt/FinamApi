1cb940f26b5888f96bceffbf1e9e7af08f553875
Tema <pshenichnykh.art@yandex.com>
2025-07-01 20:44:57 +0800

    Create MarketController.cs

---
+++ FinamAPI/Controllers/MarketController.cs
@@ -0,0 +1,17 @@
+﻿using FinamAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinamAPI.Controllers
+{
+   [ApiController]
+    [Route("api/market")]
+    public class MarketController : ControllerBase
+    {
+        private readonly IMarketDataService _marketDataService;
+
+        public MarketController(IMarketDataService marketDataService)
+        {
+            _marketDataService = marketDataService;
+        }
+    }
+}
