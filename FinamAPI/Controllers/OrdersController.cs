621b696ca7119af821acf9ea7e65f299afdc3be8
Tema <pshenichnykh.art@yandex.com>
2025-07-05 20:51:17 +0800

    Create OrdersController.cs

---
+++ FinamAPI/Controllers/OrdersController.cs
@@ -0,0 +1,24 @@
+﻿using FinamAPI.Configs;
+using FinamAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace FinamAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+        private readonly FinamApiSettings _settings;
+
+        public OrdersController(
+            IOrderService orderService,
+            IOptions<FinamApiSettings> settings)
+        {
+            _orderService = orderService;
+            _settings = settings.Value;
+        }
+
+    }
+}
