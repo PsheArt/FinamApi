17ae0faf7d9873768da29cff392722c447fbb870
Tema <71920004+PsheArt@users.noreply.github.com>
2025-08-04 20:01:19 +0800

    Update Auth.cs

--- FinamAPI/Models/Auth.cs
+++ FinamAPI/Models/Auth.cs
@@ -6,21 +6,21 @@ namespace FinamAPI.Models
     {
 
         [JsonPropertyName("client_secret")]
-        public string ClientSecret { get; set; }
+        public string? ClientSecret { get; set; }
     }
     public class AuthResponse
     {
         [JsonPropertyName("access_token")]
-        public string AccessToken { get; set; }
+        public string? AccessToken { get; set; }
 
         [JsonPropertyName("token_type")]
-        public string TokenType { get; set; } = "Bearer";
+        public string? TokenType { get; set; } = "Bearer";
 
         [JsonPropertyName("expires_in")]
         public int ExpiresIn { get; set; }
 
         [JsonPropertyName("scope")]
-        public string Scope { get; set; }
+        public string? Scope { get; set; }
 
         [JsonIgnore]
         public DateTime TokenExpiration { get; set; }
@@ -32,9 +32,9 @@ namespace FinamAPI.Models
     public class AuthErrorResponse
     {
         [JsonPropertyName("error")]
-        public string Error { get; set; }
+        public string? Error { get; set; }
 
         [JsonPropertyName("error_description")]
-        public string ErrorDescription { get; set; }
+        public string? ErrorDescription { get; set; }
     }
 }
